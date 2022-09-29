118db15288c967f8834e99c70c6680b5932f5ee8
dawunsk <114541237+dawunsk@users.noreply.github.com>
2022-09-29 16:45:12 +0800

    Add files via upload

---
+++ 22.9.29.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#define STOP '#'
+//
+//int main(void)
+//{
+//    int ch;
+//    int i = 0;
+//
+//    printf("Enter some characters('#' to quit):\n");
+//    while ((ch = getchar()) != STOP)
+//    {
+//        if (i++ % 8 == 0)
+//        {
+//            putchar('\n');
+//        }
+//        if (ch == '\n')
+//        {
+//            printf("'\\n'-%3d ", ch);
+//        }
+//        else if (ch == '\t')
+//        {
+//            printf("'\\t'-%3d ", ch);
+//        }
+//        else
+//        {
+//            printf("'%c'-%3d ", ch, ch);
+//        }
+//    }
+//    printf("Done.\n");
+//
+//    return 0;
+//}
