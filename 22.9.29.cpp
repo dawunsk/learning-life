95fa82260182b799442ddf88cc6c72205569bebf
dawunsk <114541237+dawunsk@users.noreply.github.com>
2022-09-30 16:17:27 +0800

    Add files via upload

--- 22.9.29.cpp
+++ 22.9.29.cpp
@@ -1,33 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-//#include <stdio.h>
-//#define STOP '#'
-//
-//int main(void)
+//#include<stdio.h>
+//int main()
 //{
-//    int ch;
-//    int i = 0;
-//
-//    printf("Enter some characters('#' to quit):\n");
-//    while ((ch = getchar()) != STOP)
-//    {
-//        if (i++ % 8 == 0)
-//        {
-//            putchar('\n');
-//        }
-//        if (ch == '\n')
-//        {
-//            printf("'\\n'-%3d ", ch);
-//        }
-//        else if (ch == '\t')
-//        {
-//            printf("'\\t'-%3d ", ch);
-//        }
-//        else
-//        {
-//            printf("'%c'-%3d ", ch, ch);
-//        }
-//    }
-//    printf("Done.\n");
-//
-//    return 0;
+//	int ch = 0;
+//	int count = 0;
+//	while((ch=getchar()) != '#')
+//	{
+//		if ((ch == ' ' )|| (ch == '\n'))
+//			count++;
+//		else
+//			count++;
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int ch = 0;;
+//	int count = 0;
+//	while ((ch = getchar()) != '#')
+//	{
+//		printf("%c-%d", ch, ch);
+//		count++;
+//		if (count == 8)
+//			printf("\n");
+//	}
+//	return 0;
 //}
