d2d9329f62e4f8c2f82614928d5838cafc439413
dawunsk <114541237+dawunsk@users.noreply.github.com>
2022-10-05 10:55:04 +0800

    Add files via upload

---
+++ 22.10.05.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[10] = { -1,-2,-3,-4,-5,-6,-7,-8,-9,-10 };
+//	int i = 0;
+//	int max = arr[0];
+//	for (i = 0; i < 10; i++)
+//	{
+//		if (arr[i] > max)
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("%d", max);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i <= 9; i++)//ÐÐ
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%2d ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//
+//
+//	return 0;
+//}
