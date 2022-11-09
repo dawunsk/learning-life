95881c64b2db0ce0f186a86d90893bb296560552
dawunsk <114541237+dawunsk@users.noreply.github.com>
2022-11-09 10:06:54 +0800

    Add files via upload

---
+++ aaa.cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//void change(double arr[5], int n)
+//{
+//	int i = 0;
+//	double a = 0;
+//	for (i = 0; i < n / 2; i++)
+//	{
+//		a = arr[i];
+//		arr[i] = arr[n - 1 - i];
+//		arr[n - 1 - i] = a;
+//	}
+//}
+//void show(double arr[5], int n)
+//{
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		printf("%g ", arr[i]);
+//	}
+//}
+//int main()
+//{
+//	double arr[5] = { 1.1,2.2,3.3,4.4,5.5 };
+//	change(arr, 5);
+//	show(arr, 5);
+//	return 0;
+//}
+//#define N 4
+//void sum(int arr1[], int arr2[], int arr3[], int n)
+//{
+//	int i = 0;
+//	for (i = 0; i < N; i++)
+//	{
+//		arr3[i] = arr1[i] + arr2[i];
+//	}
+//}
+//void show(int arr1[], int arr2[], int arr3[], int n)
+//{
+//	int j = 0;
+//	for (j = 0; j < N; j++)
+//	{
+//		printf("%d ", arr3[j]);
+//	}
+//}
+//int main()
+//{
+//	int arr1[N] = { 2,4,5,8 };
+//	int arr2[N] = { 1,0,4,6 };
+//	int arr3[N] = { 0 };
+//	
+//	sum(arr1, arr2, arr3, N);
+//	show(arr1, arr2, arr3, N);
+//	return 0;
+//}
